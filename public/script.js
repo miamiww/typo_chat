dcdefd06d881d476d35991e45baf43170551b13a
Alden Jones <rivendalejones@gmail.com>
2019-09-30 19:37:05 -0400

    chatting works but the typo element doesn't

--- public/script.js
+++ public/script.js
@@ -1,32 +1,10 @@
-//** */ GLOBAL VARIABLES **//
-let canvas;
-//dom elements
-let nameInput, msgInput;
-let sendButton;
-let userReq;
-//element selection
-let chatbody;
-let chatWindow;
-//msg attributes
-let urUsername;
-let msgCount = 0;
-//boolean checks
-let nameSubmitted = false;
-//sockets
-let socket;
-//data for loading past conversation
-let newinfo;
-let messagecount;
-//info for incoming messages
-let newMsgData;
-let typoMessage = "";
-
+//GLOBAL VARIABLES
 //msg attributes
 let username;
 //load message history once
 let historyLoaded = false;
 //text area
-
+let msgInput;
 
 //setting up the socket for local testing
 socket = io.connect("http://localhost:8000");
@@ -34,18 +12,40 @@ socket = io.connect("http://localhost:8000");
 // socket = io.connect("http://prototypes.alden.website:8000")
 
 //get broadcasted text & post to browser
+//receive conversation history only once
+socket.on('convoHistory', function (data) {
+  if (!historyLoaded) {
+    for (let i = 0; i < data.length; i++) {
+      //add messages to chat body
+      postMessage(data[i]);
+    }
+    historyLoaded = true;
+  }
+});
+
+//get broadcasted text & post to browser
+socket.on('chatmsg', function (data) {
+  console.log("got msg", data);
+  postMessage(data);
+  if (username == data.user) {
+    //clear input for current user
+    msgInput.value = "";
+  }
+
+});
+
 
 
 window.addEventListener('load', function () {
   //get dom elements
-  chatWindow = document.getElementById('chatbody');
-  msgInput = document.getElementById('msgInput');
   let nameInput = document.getElementById('nameInput');
   let nameAlert = document.getElementById('nameAlert');
-  chatbody = document.getElementById('chatbody');
+  let chatbody = document.getElementById('chatbody');
+  msgInput = document.getElementById('msgInput');
   let sendButton = document.getElementById('sendButton');
 
 
+  //get user's username
   nameInput.addEventListener('keyup', function (e) {
     e.preventDefault();
     if (e.keyCode === 13) {
@@ -62,130 +62,20 @@ window.addEventListener('load', function () {
         console.log("must submit text");
       }
     }
-
-    msgInput.addEventListener('keyup', function (e) {
-      if (e.keyCode === 13) {
-        e.preventDefault();
-        //remove extra new line
-        msgInput.value = msgInput.value.replace(/\n/g, '');
-        newMessage();
-      }
-    });
-    sendButton.addEventListener('click', newMessage, false);
-  })
-
-  socket.on('chatmsg', function(data) {
-    let posttext = data.user + " (" + convertDate(data.time) + "): " + data.msg;
-    let p = createP(posttext);
-    chatbody.child(p);
-    chatWindow.scrollTo(0, chatWindow.scrollHeight);
   });
-});
 
-function setup() {
-  noCanvas();
-  loadJSON('convo.json', getOldConvo);
-
-}
-
-// newText adds to the chat body any new text that is in the message input form. this allows in our functioning the pasting of links or of other texts
-function newText() {
-  if (trim(msgInput.value()) != "") {
-    loadJSON('convo.json', updateJSON);
-    let message = msgInput.value();
-    //create object of msg data
-    newMsgData = {
-      time: new Date().getTime() / 1000,
-      msg: message,
-      length: message.length,
-      user: urUsername
+  //hit enter of send a message via hit send
+  msgInput.addEventListener('keyup', function (e) {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      //remove extra new line
+      msgInput.value = msgInput.value.replace(/\n/g, '');
+      sendMessage();
     }
-    //   //emit to other viewers
-      socket.emit('chatmsg', newMsgData);
-    //   //post to chat
-      let posttext = newMsgData.user + " (" + convertDate(newMsgData.time) + "): " + newMsgData.msg;
-      let p = createP(posttext);
-      chatbody.appendChild(p);
-      cleartext();
-    // }
-  }
-  return false;
-}
+  });
+  sendButton.addEventListener('click', sendMessage, false);
 
-function newMessage(message){
-  loadJSON('convo.json', updateJSON);
-  newMsgData = {
-    time: new Date().getTime() / 1000,
-    msg: message,
-    length: message.length,
-    user: urUsername
-  }
-  socket.emit('chatmsg', newMsgData);
-  let posttext = newMsgData.user + " (" + convertDate(newMsgData.time) + "): " + newMsgData.msg;
-  let p = createP(posttext);
-  chatbody.appendChild(p);
-  cleartext();
-  chatWindow.scrollTo(0, chatWindow.scrollHeight);
-}
-
-function textStream(message,singleChar) {
-    message = message+singleChar//msgInput.value();
-    //create object of msg data
-    return message;
-}
-
-function updateJSON(data) {
-  newinfo = data;
-  messagecount = Object.keys(newinfo).length;
-  lastInterval = newinfo[messagecount - 1].time - newinfo[messagecount - 2].time;
-  lastLength = newinfo[messagecount -1].length;
-  console.log('json file loaded');
-}
-
-function getOldConvo(data) {
-  //pass data to newinfo
-  newinfo = data;
-  messagecount = Object.keys(newinfo).length;
-  //show all old messages
-  for (let i = 1; i < messagecount; i++) {
-    let posttext = newinfo[i].user + " (" + convertDate(newinfo[i].time) + "): " + newinfo[i].msg;
-    let p = createP(posttext);
-    chatbody.appendChild(p);
-  }
-  chatWindow.scrollTo(0, chatWindow.scrollHeight);
-}
-
-
-
-  //enter key will trigger send
-function keyPressed() {
-    if (keyCode == ENTER && nameSubmitted == true&& typoMessage !="") {
-      // newText();
-      newMessage(typoMessage);
-      return false;
-    } 
-}
-
-function keyTyped(){
-  if(nameSubmitted==true){
-    // newText_stream(key);
-    ypoMessage = textStream(typoMessage,key)
-  }
-}
-  
-  //clear text/make new text area after sending text
-function cleartext() {
-  msgInput.remove();
-  msgInput = createElement('textarea', '')
-  msgInput.id('msgInput');
-  typoMessage = "";
-}
-
-  //convert to date & time
-function convertDate(epochdate) {
-    let myDate = new Date(epochdate * 1000);
-    return myDate.toLocaleString();
-}
+});
 
 
 function sendMessage() {
@@ -204,4 +94,31 @@ function sendMessage() {
     //send message to server
     socket.emit('chatmsg', newMessage);
   }
+}
+
+function postMessage(chatMsgObj) {
+  let posttext = chatMsgObj.user + " (" + convertDate(chatMsgObj.time) + "): " + chatMsgObj.msg;
+  //create a paragraph element
+  let p = document.createElement('p');
+  p.innerText = posttext;
+  //add to chatbody
+  chatbody.appendChild(p);
+  //scroll to bottom
+  chatbody.scrollTo(0, chatbody.scrollHeight);
+}
+
+function postError(errTxt) {
+  //create a paragraph element
+  let p = document.createElement('p');
+  p.className = "warning";
+  p.innerText = errTxt;
+  //add to chatbody
+  chatbody.appendChild(p);
+  //scroll to bottom
+  chatbody.scrollTo(0, chatbody.scrollHeight);
+}
+
+function convertDate(epochdate) {
+  let myDate = new Date(epochdate * 1000);
+  return myDate.toLocaleString();
 }
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-var express = require('express');
-var app = express();
-var server = app.listen(8000);
+//set up express
+let express = require("express");
+let app = express();
+//demo local
+// let server = app.listen(8000);
+let server = app.listen(8000);
+app.use(express.static("public"));
 
 
 //file system
@@ -8,8 +12,6 @@ let fs = require('fs');
 const url = require('url');
 var Datastore = require('nedb');
 
-
-app.use(express.static('public'));
 
 app.get("/", function (request, response) {
 	  response.sendFile(__dirname +'/views'+'/index.html');
@@ -27,35 +29,69 @@ app.get("/therealdeal-chat", function (request, response) {
   response.sendFile(__dirname +'/views'+'/secret-chat.html');
 });
 
-let convo, data;
-let exists = fs.existsSync('public/convo.json');
-if (exists) {
-  console.log('loading conversation');
-  data = fs.readFileSync('public/convo.json');
-  convo = JSON.parse(data);
-} else {
-  console.log('no conversation available');
-  convo = {};
-}
-
+//set up sockets
 let socket = require("socket.io");
 let io = socket(server);
-io.sockets.on("connection", newConnection);
 
-function newConnection(socket) {
+
+// when new client connects
+io.sockets.on("connection", function (socket) {
   console.log("new connection: " + socket.id);
-  socket.on('chatmsg', function(data) {
-    //data is the incoming object with each message's data
-    //add to the json object
-    let count = Object.keys(convo).length;
-    convo[count] = data;
-    //write to json file
-    let json = JSON.stringify(convo, null, 2);
-    fs.writeFile('public/convo.json', json, finished);
-    function finished(err) {
-      console.log('it worked');
+  //variable for this socket's database & room
+  let db = new Datastore({
+    filename: "convo-public.db",
+    autoload: true
+  });
+
+  db.find({}).sort({ time: 1 }).exec(function (err, docs) {
+    if (err != null) {
+      console.log("err:" + err);
+    } else if (docs.length < 2) {
+
+    } else {
+      //send conversatino history to newly connected client
+      console.log("message history retreived");
+      socket.emit('convoHistory', docs);
+    }
+  });
+  
+
+  //when server receives a message
+  socket.on('chatmsg', function (data) {
+    //reload database
+    let db = new Datastore({
+      filename: "convo-public.db",
+      autoload: true
+    });
+
+    let errMsg = {
+      timeErr: false,
+      timeDifference: null,
+      lengthErr: false,
+      lengthDifference: null
+    }
+    emitMessage(data, db);
+    //query for longest message
+  });
+
+  //notify when user disconnects
+  socket.on('disconnect', function () {
+    console.log("Client has disconnected " + socket.id);
+  });
+
+});
+
+function emitMessage(data, db) {
+  //send text message to all users (including the sender)
+  io.emit('chatmsg', data);
+  // add to database
+  db.insert(data, function (err, newDocs) {
+    if (err != null) {
+      console.log("err:" + err);
+    } else {
+      console.log("incoming msg: " + newDocs.msg);
     }
     //broadcast text message change to all users
-    socket.broadcast.emit('chatmsg', data);
+    // socket.broadcast.emit('chatmsg', data);
   });
 }
