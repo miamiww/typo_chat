4fd03da763eb6a06afe9abc158a282f11ea5c33b
Alden Jones <rivendalejones@gmail.com>
2019-11-24 19:15:38 -0500

    adding 3js cube

--- public/cones.js
+++ public/cones.js
@@ -1,20 +1,21 @@
-console.log('working');
+// console.log('working');
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( 100, 100 );
-document.body.appendChild(renderer.domElement );
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0xff00fe } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
-var geometry = new THREE.ConeGeometry( 5, 20, 32 );
-var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
-var cone = new THREE.Mesh( geometry, material );
-scene.add( cone );
+camera.position.z = 5;
 
 function animate() {
-	requestAnimationFrame( animate );
+    requestAnimationFrame( animate );
     renderer.render( scene, camera );
-    console.log('working');
-
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
 }
 animate();
